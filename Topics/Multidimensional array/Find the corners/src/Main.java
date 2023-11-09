4e10fa9a2fb1e3576e5f320c96e12a0b7221b157
Cuninka Pavol <pavol.cuninka@icloud.com>
2023-11-09 12:07:32 +0100

    last commit every taks and test work correctly

--- Topics/Multidimensional array/Find the corners/src/Main.java
+++ Topics/Multidimensional array/Find the corners/src/Main.java
@@ -1,6 +1,15 @@
 class ArrayOperations {
     public static void printCorners(int[][] twoDimArray) {
         // write your code here
+        int rows = twoDimArray.length;
+        int columns = twoDimArray[0].length;
 
+        int topLeft = twoDimArray[0][0];
+        int topRight = twoDimArray[0][columns - 1];
+        int bottomLeft = twoDimArray[rows - 1][0];
+        int bottomRight = twoDimArray[rows - 1][columns - 1];
+
+        System.out.println(topLeft + " " + topRight);
+        System.out.println(bottomLeft + " " + bottomRight);
     }
 }
